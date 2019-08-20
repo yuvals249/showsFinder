0bc48f276c2c8cffb49f5f1eec1a434d3d375ed0
guter <ithayg@gmail.com>
2019-08-20 12:20:30 +0300

    filters

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'mobx-react'
 import * as serviceWorker from './serviceWorker';
+import {filterstore as filterstore} from './stores/filterstore'
+import {datastore as datastore} from './stores/datastore'
+const Filterstore=new filterstore()
+const DataStore=new datastore()
+const stores={Filterstore,DataStore}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider {...stores} ><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
