f7bfaf1ca5a001a99f5b864122243416eb253412
Nathan Kleshitsky <nathankls97@gmail.com>
2019-08-20 18:58:43 +0300

    Added Changes So we All Match

--- frontend/src/components/Show.js
+++ frontend/src/components/Show.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import ShowPage from './ShowPage'
+import { observer, inject } from 'mobx-react'
+
+
+@inject("Datastore")
+@observer
 
 class Show extends Component {
+
+    updateShowInfo = () => {
+        this.props.Datastore.updateShowInfo(this.props.show)
+        // console.log(this.props.Datastore.showInfo)
+    }
+
     render() {
         return (
             <center>
@@ -11,6 +24,7 @@ class Show extends Component {
                         <span className='showDate'>{this.props.show.date}</span>
                         <span className='showLoc'>{this.props.show.address}</span>
                         <div className='showPriceBuyNow'>{this.props.show.currentPrice} <i class="fas fa-shekel-sign"></i> <br></br> {this.props.show.amountLeftPretty} <br></br>Buy Now</div>
+                        <Link to="/showPage"><div className='showPriceBuyNow' onClick={this.updateShowInfo}>{this.props.show.currentPrice} <i class="fas fa-shekel-sign"></i> <br></br> {this.props.show.amountLeftPretty} <br></br>Buy Now</div></Link>
                     </div>
                 </div>
             </center>
