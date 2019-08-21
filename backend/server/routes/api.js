af9c9c9f722441bebf89bec957a18b0a7c335110
yuval schwartz <yuvals249@gmail.com>
2019-08-21 14:43:54 +0300

    added video id each show

--- backend/server/routes/api.js
+++ backend/server/routes/api.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const request = require('request-promise')
 const Show = require('../model/Show')
+const translate = require('translate');
+translate.engine = 'yandex';
 const apiKey = 'AIzaSyCP-CH_oc8w_eAgbSFTOVphBrCY0OzFGwc'
+
 
 // save all the data in database
 // let data = require('../routes/data') 
@@ -18,13 +21,29 @@ router.get('/', (req, res) => {
     })
 })
 
-// router.get('/:name', async (req, res) => {
-//     let name = req.params.name
-//     console.log(name)
-//     let response = await request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=caveret&key=AIzaSyCP-CH_oc8w_eAgbSFTOVphBrCY0OzFGwc`)
-//     response = JSON.parse(response)
-//     console.log(response.items[0])
-// })
+router.put(`/:name`, (req,res) => {
+    let name = req.params.name
+    translate(name,
+        { from: 'he', to: 'en', engine: 'yandex', key: 'trnsl.1.1.20190821T090657Z.3db80ca875d9cd52.80084d1a4ebc498961c4eb0b8a1d26274dde85f2' })
+        .then(text => {
+            request.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${text}&key=${apiKey}`).then(response => {
+            let audioData = JSON.parse(response).items
+            for (let i of audioData) {
+                if (i.id.videoId) {
+                    let videoId = i.id.videoId
+                    console.log(videoId)
+                    Show.findOneAndUpdate({name: name}, {youtubeVideoId: videoId}, {new: true}, function (err, response) {
+                        return
+                    })
+                    break;
+                }
+                else {
+                    console.log('Do not find videoId')
+                }
+            }
+            })
+        });
+})
 
 
 router.post('/filter', (req, res) => {
