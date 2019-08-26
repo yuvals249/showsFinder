70c7311fb44b19a79760a827c8343a15844abc6f
Nathan Kleshitsky <nathankls97@gmail.com>
2019-08-26 16:56:55 +0300

    Added Thing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,15 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Search from './search'
 
 class Navbar extends Component {
-       constructor() {
-        super()
-        this.state = {
-            searchBar: false
-        }
-    }
-    searchBar = () => {
-        this.setState({
-            searchBar: !this.state.searchBar
-        })
-    }
     render() {
         
         return (
             <div id="nav-bar">
                 <span><Link to='/' className='logo'>ShowsFinder</Link></span>
-                <i className={this.state.searchBar ? "fas fa-angle-double-up" : "fas fa-angle-double-down"} onClick={this.searchBar}></i>
+                <Search />
                 <div className='NavLinks'>
                     <div><Link to='/SignUp' className='link' >Log In/Sign Up</Link></div>
                 </div>
-                {this.state.searchBar ? <Search /> : null}
             </div>
         )
     }
