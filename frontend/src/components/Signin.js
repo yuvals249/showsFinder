eea230185294ba31d085903d3086a758a7d7b06b
guter <ithayg@gmail.com>
2019-08-27 10:03:40 +0300

    signup chebges

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react'
 import axios from 'axios'
+
 @inject("Signinstore")
 @observer
 class SignIn extends Component {
-    
+    constructor(){
+      super()
+      this.state={
+        user:false
+      }
+    }
     handleinputs = (e) => {
 
         this.props.Signinstore.handleinputs(e.target.name, e.target.value)
         
     }
 
-  async  checkuser () {
+   checkuser=async ()=> {
     let res= await axios.get(`http://localhost:8080`)
+    localStorage.setItem('name', 'ahi')
+    this.setState({user : true})
     return res
   }
     render (){
@@ -22,7 +30,7 @@ class SignIn extends Component {
                     <input type='text' name='email' onChange={this.handleinputs} placeholder='email@example.com' ></input>
                     <input type='password' name='password' onChange={this.handleinputs} placeholder='password' ></input>
                     <button onClick={this.checkuser} >LogIn</button>
-                
+               {this.state.user ?  <div>welcome {localStorage.getItem('name')}</div>: null}
             </div>
         )
     }
--- frontend/src/stores/filterstore.js
+++ frontend/src/stores/filterstore.js
@@ -3,7 +3,7 @@ import { observable, action } from  'mobx'
 
 export class FilterStore {
     @observable name = ""
-    @observable cost = 500
+    @observable cost = 200
     @observable location = "all"
     @observable date = "all"
     @action handlefilters = (name, value) => {
